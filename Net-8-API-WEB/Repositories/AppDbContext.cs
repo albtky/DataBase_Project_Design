79478236d6a361d150f064d2155897f426b682ac
alperen <alperenttky@gmail.com>
2025-02-11 21:12:06 +0300

    .Net Projesi

---
+++ Net-8-API-WEB/Repositories/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{ 
+
+        public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; } = default!
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly()
+
+
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
